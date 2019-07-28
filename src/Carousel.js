601b591e136cb7d5b04c19b228658ed13cf43794
Dennis Myasnyankin <dennis415@live.com>
2019-07-28 13:43:20 -0700

    Init Carousel Comp

---
+++ src/Carousel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Carousel extends React.Component {
+    render () {
+        return (
+            <div className='carousel'>
+                This is the Carousel Component
+            </div>
+        );
+    }
+}
+
+export default Carousel;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Carousel from "./Carousel";
+
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+        <Carousel />
+    </div>,
+    destination
+);
