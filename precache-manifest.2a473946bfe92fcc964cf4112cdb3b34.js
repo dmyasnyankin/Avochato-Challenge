c3c8218a61cbffbdfa4acc7bfc27eebfe7a0ae49
Dennis Myasnyankin <dennis415@live.com>
2019-07-29 20:16:52 -0700

    Updates

---
+++ precache-manifest.2a473946bfe92fcc964cf4112cdb3b34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c07d97c3df75aaf11bc0707ac1c4a2",
+    "url": "/Avochato-Challenge/index.html"
+  },
+  {
+    "revision": "df08d6d8d7ab60f3803a",
+    "url": "/Avochato-Challenge/static/css/main.05ccffc3.chunk.css"
+  },
+  {
+    "revision": "3e02a8c67cfbd25b4d1a",
+    "url": "/Avochato-Challenge/static/js/2.1b5bec0c.chunk.js"
+  },
+  {
+    "revision": "df08d6d8d7ab60f3803a",
+    "url": "/Avochato-Challenge/static/js/main.30b54d8d.chunk.js"
+  },
+  {
+    "revision": "cd45484cb58813a6865b",
+    "url": "/Avochato-Challenge/static/js/runtime~main.d5c6f737.js"
+  }
+]);
